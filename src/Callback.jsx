b861d560688f294d4b9ebf5e1c3326ef5664ebd3
henrikMew <henrik@mewb.no>
2025-04-02 09:59:50 +0200

    Create Callback.jsx

---
+++ src/Callback.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Callback = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const code = params.get('code');
+    const error = params.get('error');
+
+    if (code) {
+      console.log('Got auth code:', code);
+      // TODO: Send this to your backend for token exchange
+    } else if (error) {
+      console.error('Login failed:', error);
+    } else {
+      console.warn('No code or error found in callback');
+    }
+  }, [location]);
+
+  return <div>Logging you in...</div>;
+};
+
+export default Callback;
